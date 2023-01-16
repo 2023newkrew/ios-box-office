64479bef573fb1d22f0075bcb2c4344309e888d7
john.lim <limjae96@gmail.com>
2023-01-16 21:22:11 +0900

    refactor: 미사용 print 삭제

--- BoxOfficeTests/BoxOfficeSearchResultTests.swift
+++ BoxOfficeTests/BoxOfficeSearchResultTests.swift
@@ -10,33 +10,34 @@ import XCTest
 final class BoxOfficeSearchResultParsingTests: XCTestCase {
     let data = NSDataAsset(name: "SampleData", bundle: Bundle(for: ViewController.self))?.data
     
-    func test_데이터불러오기테스트(){
+    func test_데이터불러오기테스트() {
         XCTAssertNotNil(data)
     }
     
-    func test_JSONSerialization테스트(){
+    func test_JSONSerialization테스트() {
         guard let data = data else {
             XCTAssertTrue(false)
             return
         }
-        guard let parsed = try? JSONSerialization.jsonObject(with: data, options: []) as? [String : Any] else {
+        do {
+            let parsed = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
+            XCTAssertNotNil(parsed)
+        } catch {
+            print("error",error)
             XCTAssertTrue(false)
             return
         }
-        print(parsed)
-        XCTAssertNotNil(parsed)
     }
     
-    func test_JSONDecoder로Model파싱테스트(){
+    func test_JSONDecoder로Model파싱테스트() {
         guard let data = data else {
             XCTAssertTrue(false)
             return
         }
-        do{
+        do {
             let parsed = try JSONDecoder().decode(BoxOfficeSearchResult.self, from: data)
-            print(parsed)
             XCTAssertNotNil(parsed)
-        }catch {
+        } catch {
             print("error",error)
             XCTAssertTrue(false)
             return
