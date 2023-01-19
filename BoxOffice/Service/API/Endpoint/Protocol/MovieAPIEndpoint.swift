55f79c649d5609396832f7f9c88e27fa817cadbb
forest.jay <forestsj30@gmail.com>
2023-01-19 17:41:06 +0900

    feat: MovieAPIEndpoint protocol 구현

---
+++ BoxOffice/Service/API/Endpoint/Protocol/MovieAPIEndpoint.swift
@@ -0,0 +1,12 @@
+//
+//  MovieAPIEndpoint.swift
+//  BoxOffice
+//
+//  Created by kakao on 2023/01/19.
+//
+
+import Foundation
+
+protocol MovieAPIEndpoint: APIEndpoint, MovieAPIInformationOwner { }
+
+protocol MovieAPIGetEndpoint: MovieAPIEndpoint, APIGetEndpoint { }
