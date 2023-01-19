ab97efe506950ba5c22e3fb31f38ae679c5ed778
john.lim <limjae96@gmail.com>
2023-01-19 10:48:32 +0900

    feat: 영화 상세정보 API에 사용되는 Query 구조체 추가

---
+++ BoxOffice/Network/MovieDetail/MovieDetailAPIQuery.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailAPIQuery.swift
+//  BoxOffice
+//
+//  Created by john-lim on 2023/01/17.
+//
+
+import Foundation
+
+struct MovieDetailAPIQuery: QueryItems{
+    var items: [URLQueryItem]
+    
+    init(key: String,
+         movieCode: String) {
+        var items: [URLQueryItem] = []
+        items.append(URLQueryItem(name: "key", value: key))
+        items.append(URLQueryItem(name: "movieCd", value: movieCode))
+        self.items = items
+    }
+}
