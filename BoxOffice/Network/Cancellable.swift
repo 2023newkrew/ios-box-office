5dc939084aa969305e996f7ca47f5b2259a84b7b
forest.jay <forestsj30@gmail.com>
2023-01-19 17:30:53 +0900

    feat: Cancellable 프로토콜 구현 및 URLSessionTask 익스텐션 적용

---
+++ BoxOffice/Network/Cancellable.swift
@@ -0,0 +1,12 @@
+//
+//  Cancellable.swift
+//  BoxOffice
+//
+//  Created by kakao on 2023/01/19.
+//
+
+import Foundation
+
+protocol Cancellable {
+    func cancel()
+}
---
+++ BoxOffice/Network/URLSessionTask+Cancellable.swift
@@ -0,0 +1,10 @@
+//
+//  URLSessionTask+Cancellable.swift
+//  BoxOffice
+//
+//  Created by kakao on 2023/01/19.
+//
+
+import Foundation
+
+extension URLSessionTask: Cancellable { }
