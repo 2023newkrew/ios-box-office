04e3a32ff85d05d47db071be70dccc159ee0caa5
forest.jay <forestsj30@gmail.com>
2023-01-19 17:31:09 +0900

    feat: HTTP Method 구현

---
+++ BoxOffice/Network/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  BoxOffice
+//
+//  Created by kakao on 2023/01/19.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
