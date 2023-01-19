ce0cf9c98c5fa7dcc1a7f2ebf2ba508a91c4e574
노신희 <rebecca_0922@naver.com>
2023-01-19 12:17:46 +0900

    refactor: 모델 타입 형식 통일

--- BoxOffice/Model/DailyBoxOffice.swift
+++ BoxOffice/Model/DailyBoxOffice.swift
@@ -8,21 +8,63 @@
 import Foundation
 
 struct DailyBoxOffice: Decodable {
-    struct Information: Decodable {
-        let type: String
-        let showRange: String
-        let movieList: [Movie]
-
-        enum CodingKeys: String, CodingKey {
-            case type = "boxofficeType"
-            case showRange
-            case movieList = "dailyBoxOfficeList"
-        }
-    }
-    
-    let information: Information
+    let information: DailyBoxOfficeInformation
     
     enum CodingKeys: String, CodingKey {
         case information = "boxOfficeResult"
+    }
+}
+
+struct DailyBoxOfficeInformation: Decodable {
+    let type: String
+    let showRange: String
+    let movieList: [DailyMovie]
+
+    enum CodingKeys: String, CodingKey {
+        case type = "boxofficeType"
+        case showRange
+        case movieList = "dailyBoxOfficeList"
+    }
+}
+
+struct DailyMovie: Decodable {
+    let number: String
+    let rank: String
+    let rankingChange: String
+    let rankOldOrNew: String
+    let representativeCode: String
+    let name: String
+    let openDate: String
+    let salesForTheDay: String
+    let salesShareForTheDay: String
+    let salesChange: String
+    let salesRatioChange: String
+    let cumulativeSales: String
+    let audienceCount: String
+    let audienceChange: String
+    let audienceRatioChange: String
+    let cumulativeAudience: String
+    let screenCountForTheDay: String
+    let showCountForTheDay: String
+
+    enum CodingKeys: String, CodingKey {
+        case number = "rnum"
+        case rank
+        case rankingChange = "rankInten"
+        case rankOldOrNew = "rankOldAndNew"
+        case representativeCode = "movieCd"
+        case name = "movieNm"
+        case openDate = "openDt"
+        case salesForTheDay = "salesAmt"
+        case salesShareForTheDay = "salesShare"
+        case salesChange = "salesInten"
+        case salesRatioChange = "salesChange"
+        case cumulativeSales = "salesAcc"
+        case audienceCount = "audiCnt"
+        case audienceChange = "audiInten"
+        case audienceRatioChange = "audiChange"
+        case cumulativeAudience = "audiAcc"
+        case screenCountForTheDay = "scrnCnt"
+        case showCountForTheDay = "showCnt"
     }
 }
