da39a2bc3d23ebda58993bfdfaff26723d9a6d71
john.lim <limjae96@gmail.com>
2023-01-25 13:56:22 +0900

    refactor: ViewController 명칭 변경

--- BoxOffice/ViewController/BoxOfficeListViewController.swift
+++ BoxOffice/ViewController/BoxOfficeListViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class BoxOfficeListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
 }
-
