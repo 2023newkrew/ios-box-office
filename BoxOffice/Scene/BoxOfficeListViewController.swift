1d5b473bf1b871a2737eab93e20afcf393858c61
volgaHwang <wltkak2@gmail.com>
2023-01-30 11:54:00 +0900

    feat: BoxOfficeListViewController Section 추가사항 반영

--- BoxOffice/Scene/BoxOfficeListViewController.swift
+++ BoxOffice/Scene/BoxOfficeListViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class BoxOfficeListViewController: UIViewController {
+    enum Section {
+        case main
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
