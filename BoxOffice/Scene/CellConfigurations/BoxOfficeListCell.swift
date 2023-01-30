53dd205deb174277395affba611db7a70d07ef30
volgaHwang <wltkak2@gmail.com>
2023-01-30 23:02:19 +0900

    feat: BoxOfficeListCell 내부 구조 구현
    - `accessoryImageView`를 따로 구현했습니다.
    - separator를 `UIView()`를 통해 구현했습니다.
    - 전체적인 틀은 `UIStackView`를 사용하여 구조를 잡아두었습니다.
    - 초기화 시 `configure()` 메소드를 통해 조절하게 구현했습니다.

--- BoxOffice/Scene/CellConfigurations/BoxOfficeListCell.swift
+++ BoxOffice/Scene/CellConfigurations/BoxOfficeListCell.swift
@@ -5,4 +5,134 @@
 //  Created by kakao on 2023/01/30.
 //
 
-import Foundation
+import UIKit
+
+class BoxOfficeListCell: UICollectionViewCell {
+    static let reuseIdentifier = "box-office-list-cell-reuseidentifier"
+    
+    let accessoryImageView = UIImageView()
+    
+    let separatorView = UIView()
+    
+    let stackView:  UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        
+        return stackView
+    }()
+    
+    let labelStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.layoutMargins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.alignment = .center
+        
+        return stackView
+    }()
+    
+    let rightStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
+    let titleLabel = UILabel()
+    let showCountLabel = UILabel()
+    
+    let leftStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    let rankLabel = UILabel()
+    let rankDescriptionLabel = UILabel()
+    
+    var showsSeparator = true {
+        didSet {
+            updateSeparator()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension BoxOfficeListCell {
+    func configure() {
+        rankLabel.textAlignment = .center
+        rankDescriptionLabel.textAlignment = .center
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        showCountLabel.translatesAutoresizingMaskIntoConstraints = false
+        rankLabel.translatesAutoresizingMaskIntoConstraints = false
+        rankDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        separatorView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.adjustsFontForContentSizeCategory = true
+        showCountLabel.adjustsFontForContentSizeCategory = true
+        rankLabel.adjustsFontForContentSizeCategory = true
+        rankDescriptionLabel.adjustsFontForContentSizeCategory = true
+        
+        titleLabel.numberOfLines = 0
+        showCountLabel.numberOfLines = 0
+        
+        rankLabel.font = UIFont.preferredFont(forTextStyle: .title1)
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        showCountLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        
+        separatorView.backgroundColor = .placeholderText
+                
+        let rtl = effectiveUserInterfaceLayoutDirection == .rightToLeft
+        let chevronImageName = rtl ? "chevron.left" : "chevron.right"
+        let chevronImage = UIImage(systemName: chevronImageName)
+        
+        accessoryImageView.image = chevronImage
+        accessoryImageView.tintColor = UIColor.lightGray.withAlphaComponent(0.7)
+        
+        [titleLabel, showCountLabel].forEach { subView in
+            rightStackView.addArrangedSubview(subView)
+        }
+        
+        [rankLabel, rankDescriptionLabel].forEach { subView in
+            leftStackView.addArrangedSubview(subView)
+        }
+        
+        [leftStackView, rightStackView, accessoryImageView].forEach { subStackView in
+            labelStackView.addArrangedSubview(subStackView)
+        }
+        
+        [labelStackView, separatorView]
+            .forEach { subview in
+                stackView.addArrangedSubview(subview)
+            }
+        
+        contentView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            separatorView.heightAnchor.constraint(equalToConstant: 0.5),
+            labelStackView.widthAnchor.constraint(equalTo: contentView.widthAnchor),
+            leftStackView.widthAnchor.constraint(equalTo: labelStackView.widthAnchor, multiplier: 0.2),
+            accessoryImageView.widthAnchor.constraint(equalToConstant: 13),
+            accessoryImageView.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
+    func updateSeparator() {
+        separatorView.isHidden = !showsSeparator
+    }
+}
