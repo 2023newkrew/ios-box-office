a886e5f258cb3fe0978b5300b17d7d61ed745051
volgaHwang <wltkak2@gmail.com>
2023-01-30 12:01:35 +0900

    feat: CollectionView 추가
    - List로 사용할 CollectionView를 추가했습니다.
    - createLayout 메소드를 사용해 UICollcetionView 초기화에 사용할 layout을 구현합니다.
    - ViewController 내부 메소드를 사용하기 위해 collectionView를 `lazy var` 키워드를 사용해서 선언했습니다.

--- BoxOffice/Scene/BoxOfficeListViewController.swift
+++ BoxOffice/Scene/BoxOfficeListViewController.swift
@@ -11,11 +11,38 @@ class BoxOfficeListViewController: UIViewController {
     enum Section {
         case main
     }
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: self.createLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemBackground
+        
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
 
-
+extension BoxOfficeListViewController {
+    func createLayout() -> UICollectionViewLayout {
+        let estimatedHeight = CGFloat(100)
+        let layoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                heightDimension: .estimated(estimatedHeight))
+        let item = NSCollectionLayoutItem(layoutSize: layoutSize)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: layoutSize,
+                                                       subitem: item,
+                                                       count: 1)
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10,
+                                                        leading: 10,
+                                                        bottom: 10,
+                                                        trailing: 10)
+        section.interGroupSpacing = 10
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
 }
 
