ed463298e53eb4177f6f6f126e644c714cfc3c94
volgaHwang <wltkak2@gmail.com>
2023-01-30 23:06:51 +0900

    feat: BoxOfficeItem 구현사항 반영
    - BoxOffice Item Property를 크게 5가지로 정의했습니다.
        - `title`: 영화 제목
        - `showCountInformation`: 일일 관객 / 총 관객 에 대한 정보를 담고 있는 String 구조
        - `rank`: 박스 오피스 순위에 대한 정보를 담는 프로퍼티
        - `rankDescription`: 랭크 순위에 대해서 변동사항 또는 신작일 경우에 대한 Description 글자 색상 변경사항이 있으므로 NSAttributedString

--- BoxOffice/Entity/BoxOfficeItem.swift
+++ BoxOffice/Entity/BoxOfficeItem.swift
@@ -6,3 +6,23 @@
 //
 
 import UIKit
+
+struct BoxOfficeItem: Hashable {
+    let title: String
+    let showCountInformation: String
+    let rank: String
+    let rankDescription: NSAttributedString
+    
+    private let identifier = UUID()
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(identifier)
+    }
+    
+    init(title: String, showCountInformation: String, rank: String, rankDescription: NSAttributedString) {
+        self.title = title
+        self.showCountInformation = showCountInformation
+        self.rank = rank
+        self.rankDescription = rankDescription
+    }
+}
