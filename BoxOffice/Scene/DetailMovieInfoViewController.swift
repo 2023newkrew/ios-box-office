c5b312e929b2b3930b3228a404540f24b7cbfa00
volgaHwang <wltkak2@gmail.com>
2023-02-03 10:49:39 +0900

    feat: 자세한 영화 정보 조회를 위한 뷰컨트롤러 추가

---
+++ BoxOffice/Scene/DetailMovieInfoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailMovieInfoViewController.swift
+//  BoxOffice
+//
+//  Created by kakao on 2023/02/03.
+//
+
+import UIKit
+
+class DetailMovieInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
