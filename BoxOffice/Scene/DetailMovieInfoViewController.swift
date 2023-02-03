abb507487261083e4395f10ad0b4ff3b771fb391
volgaHwang <wltkak2@gmail.com>
2023-02-03 11:54:41 +0900

    feat: DetailMoviewInfoViewController에서 뷰 구성에 필요한 프로퍼티 추가 반영

--- BoxOffice/Scene/DetailMovieInfoViewController.swift
+++ BoxOffice/Scene/DetailMovieInfoViewController.swift
@@ -8,8 +8,53 @@
 import UIKit
 
 class DetailMovieInfoViewController: UIViewController {
+    private enum SubjectInfo {
+        static let director = "감독"
+        static let produceYear = "제작년도"
+        static let openDate = "개봉일"
+        static let showTime = "상영시간"
+        static let grade = "관람등급"
+        static let nation = "제작국가"
+        static let genre = "장르"
+        static let actors = "배우"
+    }
     private let movieCode: String
     private let movieName: String
+    
+    private let scrollView = UIScrollView()
+    private let mainStackView = UIStackView()
+    
+    private let directorNameStackView = UIStackView()
+    private let directorNameLabel = UILabel()
+    private let directorNameContentLabel = UILabel()
+    
+    private let produceYearStackView = UIStackView()
+    private let produceYearLabel = UILabel()
+    private let produceYearContentLabel = UILabel()
+    
+    private let openDateStackView = UIStackView()
+    private let openDateLabel = UILabel()
+    private let openDateContentLabel = UILabel()
+    
+    private let showTimeStackView = UIStackView()
+    private let showTimeLabel = UILabel()
+    private let showTimeContentLabel = UILabel()
+
+    private let gradeStackView = UIStackView()
+    private let gradeLabel = UILabel()
+    private let gradeContentLabel = UILabel()
+    
+    private let nationStackView = UIStackView()
+    private let nationLabel = UILabel()
+    private let nationContentLabel = UILabel()
+    
+    private let genreStackView = UIStackView()
+    private let genreLabel = UILabel()
+    private let genreContentLabel = UILabel()
+    
+    private let actorStackView = UIStackView()
+    private let actorLabel = UILabel()
+    private let actorContentLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
