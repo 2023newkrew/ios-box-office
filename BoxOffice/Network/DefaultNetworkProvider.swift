170456293a2f807d01e04504a460c6b71ec4ec0a
forest.jay <forestsj30@gmail.com>
2023-02-07 20:52:38 +0900

    fix: 불필요 코드 삭제

--- BoxOffice/Network/DefaultNetworkProvider.swift
+++ BoxOffice/Network/DefaultNetworkProvider.swift
@@ -9,5 +9,4 @@ import Foundation
 
 final class DefaultNetworkProvider: NetworkProvider {
     let session: URLSessionable = URLSession.shared
-    let decoder: DecodingUtility = JSONDecodingUtility()
 }
