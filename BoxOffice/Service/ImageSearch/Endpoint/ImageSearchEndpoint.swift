49836aa277fb4985e7161d6781b906cbd39dd073
forest.jay <forestsj30@gmail.com>
2023-02-08 10:41:18 +0900

    refactor: Endpoint에 init을 정의

--- BoxOffice/Service/ImageSearch/Endpoint/ImageSearchEndpoint.swift
+++ BoxOffice/Service/ImageSearch/Endpoint/ImageSearchEndpoint.swift
@@ -14,24 +14,22 @@ struct ImageSearchEndpoint: ImageSearchAPIEndpoint {
         case accuracy
         case recency
     }
+    
+    init(query: String, sort: SortDescription = .accuracy, page: Int = 1, size: Int = 1) {
+        self.queries = [
+            "query": query,
+            "sort": sort.rawValue,
+            "page": String(page),
+            "size": String(size)
+        ]
+    }
 
     let method: HTTPMethod = .get
     let path: String = "image"
-    let query: String
-    let sortDescription: SortDescription = .accuracy
-    let page: Int = 1
-    let size: Int = 1
     var headers: [String: String]? {
         return ["Authorization": "KakaoAK" + " \(self.serviceKey)"]
     }
-    var queries: [String: String?] {
-        return [
-            "query": self.query,
-            "sort": self.sortDescription.rawValue,
-            "page": String(self.page),
-            "size": String(self.size)
-        ]
-    }
-    
+
+    let queries: [String: String?]
     let body: Data? = nil
 }
